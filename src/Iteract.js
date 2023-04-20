6e7885f659841b7690d6492e4d7c0431375d0f5e
dysentry30 <bagassatrianurwinanto@gmail.com>
2023-04-21 01:49:40 +0700

    feat(iteract.js): add `values` function

--- src/Iteract.js
+++ src/Iteract.js
@@ -154,6 +154,199 @@ class Iteract {
     }
 
     /**
+     * The function "take" returns a specified number of elements from an array.
+     * @param number - The number of items to be returned from the array. The `take` method returns a
+     * new array containing the first `number` items from the original array.
+     * @returns The `take` method is returning an array of elements from the beginning of the array.
+     */
+    take(number) {
+        if (number > this.length()) throw new Error("Make sure parameter number is less than the length of data")
+        return this.all().slice(0, number);
+    }
+
+    /**
+     * The function converts data to a JSON string and throws an error if the data is not valid JSON.
+     * @returns a JSON string representation of the object.
+     */
+    toJson() {
+        try {
+            return JSON.stringify(this.all());
+        } catch (e) {
+            throw new Error("Data is not valid JSON. Cannot use toJson function.");
+        }
+    }
+
+    /**
+     * The function joins the elements of an array into a string with optional separators.
+     * @param [separator] - The separator parameter is a string that will be used to separate each
+     * element in the array when they are joined together into a single string. If no separator is
+     * provided, an empty string will be used as the default separator.
+     * @param [lastElementSeparator] - lastElementSeparator is an optional parameter that specifies the
+     * separator to be used for the last element in the array when joining the elements together into a
+     * string. If this parameter is not provided, the separator parameter will be used for all elements
+     * including the last one.
+     * @returns a string that is the concatenation of all the elements in the array, separated by the
+     * `separator` parameter. If the array has more than one element, the second to last element will
+     * be separated by the `lastElementSeparator` parameter. If `lastElementSeparator` is not provided,
+     * the `separator` parameter will be used instead. If the array has only one element
+     */
+    join(separator = "", lastElementSeparator = "") {
+        if (Array.isArray(this.all())) {
+            if (this.length() == 1) {
+                return this.data[0] + "";
+            }
+            let joinedString = "";
+            for (let i = 0; i < this.data.length; i += 1) {
+                if (i == this.data.length - 2) {
+                    if (lastElementSeparator) {
+                        joinedString += this.data[i] + lastElementSeparator;
+                    } else {
+                        joinedString += this.data[i] + separator;
+                    }
+                } else if (i == this.data.length - 1) {
+                    joinedString += this.data[i];
+                } else {
+                    joinedString += this.data[i] + separator;
+                }
+            }
+            return joinedString;
+        }
+        throw new Error("join function cannot use in object");
+    }
+
+    /**
+     * This function groups an array of objects by a specified key and returns a new Iteract object
+     * with the grouped data.
+     * @param key - The key parameter is the property name that will be used to group the elements of
+     * the array. It should be a string representing the name of the property.
+     * @returns a new instance of the Iteract class with the data grouped by the specified key.
+     */
+    groupBy(key) {
+        let result = {};
+        if (key) {
+
+            if (!isFinite(key) && this.hasKeys() && this.has(key)) {
+                for (let i = 0; i < this.length(); i += 1) {
+                    const groupedKey = this.data[i][key];
+                    if (result[groupedKey]) {
+                        result[groupedKey].push(this.data[i]);
+                    } else {
+                        result[groupedKey] = [this.data[i]];
+                    }
+                }
+                return new Iteract(result);
+            } else {
+                throw new Error("Parameter must be string not number or the key is not exist in this array.")
+            }
+        }
+        throw new Error("Should pass the parameter with existing keys.");
+    }
+
+    /**
+     * The function returns the minimum value of either a specific key in an array of objects or the
+     * entire array if all values are numeric.
+     * @param [key] - The key parameter is a string that represents the property name of the object in
+     * the array. If the key parameter is not
+     * provided or is an empty string, the function will attempt to find the minimum value of the
+     * entire array.
+     * @returns the minimum value of either the specified key in the array or the minimum value of all
+     * numeric values in the array. If the data is not numeric or the specified key does not exist in
+     * the array, an error is thrown.
+     */
+    min(key = "") {
+        if (key && !isFinite(key) && this.hasKeys() && this.has(key)) {
+            let bestMinValue = Infinity;
+            for (let i = 0; i < this.length(); i++) {
+                const value = this.data[i][key];
+                bestMinValue = Math.min(bestMinValue, value);
+            }
+            return bestMinValue;
+        }
+        if (this.all().every(item => isFinite(item))) {
+            return Math.min(...this.all());
+        }
+        throw new Error("Data must be numeric only or the key is not exist in this array.")
+    }
+
+    /**
+     * The function returns the maximum value of either a specific key in an array of objects or the
+     * maximum value of all numeric values in the array.
+     * @param key - The key parameter is a string that represents the name of the property in the
+     * objects contained in the array that should be used to determine the maximum value.
+     * @returns the maximum value of either a specific key in the array or all the numeric values in
+     * the array. If the data is not numeric or the key does not exist in the array, an error is
+     * thrown.
+     */
+    max(key) {
+        if (key && !isFinite(key) && this.hasKeys() && this.has(key)) {
+            let bestMaxValue = -Infinity;
+            for (let i = 0; i < this.length(); i++) {
+                const value = this.data[i][key];
+                bestMaxValue = Math.max(bestMaxValue, value);
+            }
+            return bestMaxValue;
+        }
+        if (this.all().every(item => isFinite(item))) {
+            return Math.max(...this.all());
+        }
+        throw new Error("Data must be numeric only or the key is not exist in this array.")
+    }
+
+    /**
+     * The function calculates the sum of either all numeric values in an array or a specific key's
+     * numeric values in an array of objects.
+     * @param key - The key parameter is a string representing the key of the property to be summed up
+     * in the array.
+     * @returns the sum of all the values in the array, or the sum of a specific key's values in the
+     * array if the key exists and all values are numeric. If the data is not numeric or the key does
+     * not exist in the array, an error is thrown.
+     */
+    sum(key) {
+        if (key && !isFinite(key) && this.hasKeys() && this.has(key)) {
+            return this.all().reduce((a, b) => {
+                if(a[key]) {
+                    return a[key] + b[key];
+                }
+                return a + b[key];
+            });
+        }
+        if (this.all().every(item => isFinite(item))) {
+            return this.all().reduce((a, b) => a + b);
+        }
+        throw new Error("Data must be numeric only or the key is not exist in this array.")
+    }
+
+    /**
+     * This function calculates the average of either all numeric values in an array or the values of a
+     * specific key in an array of objects.
+     * @param key - The key parameter is an optional argument that specifies the key of the property to
+     * calculate the average for. If provided, the method will calculate the average of the values of
+     * that property for all objects in the array. If not provided, the method will calculate the
+     * average of all values in the array.
+     * @returns The `avg` function returns the average value of either the entire array or a specific
+     * key in the array. If the data is not numeric or the key does not exist in the array, it throws
+     * an error.
+     */
+    avg(key) {
+        if (key && !isFinite(key) && this.hasKeys() && this.has(key)) {
+            return this.sum(key) / this.length();
+        }
+        if (this.all().every(item => isFinite(item))) {
+            return this.sum() / this.length();
+        }
+        throw new Error("Data must be numeric only or the key is not exist in this array.")
+    }
+
+    values() {
+        let result = [];
+        for (let i = 0; i < this.length(); i+=1) {
+            const item = this.data[i];
+            result.push(Object.values(item));
+        }
+        return new Iteract(result.flat());
+    }
+
+    /**
      * The function fetches data from a given URL and returns a new Iteract object.
      * @param url - The URL from which data needs to be fetched asynchronously.
      * @returns A new instance of the `Iteract` class with the data fetched from the provided URL.
